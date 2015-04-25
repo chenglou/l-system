bcc732e65ee50e6d01676244e9a9fb7672a0108c
Cheng Lou <chenglou92@gmail.com>
2015-04-25 03:24:25 -0400

    Add webpack and babel

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    index: './index.jsx',
+  },
+  output: {
+    path: './build',
+    filename: 'index.js',
+  },
+  module: {
+    loaders: [
+      {test: /\.jsx?$/, exclude: /build|node_modules/, loader: 'babel-loader', stage: 0},
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  }
+};
+
